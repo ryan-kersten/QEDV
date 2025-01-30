7c183505fca8f17a9d3d0a0d4620edee0a2e4536
RyanI70I <ryankers@umich.edu>
2025-01-30 12:50:21 -0500

    Add syndrome class

---
+++ QEDV_lib/syndrome.py
@@ -0,0 +1,30 @@
+from QEDV_lib.stabilizer import Stabilizer
+class Syndrome:
+
+    def __init__(self, error, noerror):
+        """
+
+        :param qubits: Set of ints, with the ints representing the qubits in the stabilizer
+        """
+        if not isinstance(error, set) or not isinstance(noerror, set):
+            raise TypeError("'error' and 'noerror' must be a set.")
+
+        if not all(isinstance(el, (Stabilizer)) for el in error):
+            raise ValueError("All elements in error must be stabilizer.")
+        if not all(isinstance(el, (Stabilizer)) for el in noerror):
+            raise ValueError("All elements in noerror must be stabilizer.")
+
+        self.errors = error
+        self.noerror = noerror
+
+        """
+        Return a set of stabilizers that detected an error
+        """
+    def getErrors(self):
+        return self.error
+
+    """
+    Return a set of stabilizers that haven't detected an error
+    """
+    def getErrors(self):
+        return self.noerror
