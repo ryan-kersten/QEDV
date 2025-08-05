3567669d208fb9cb652db8ad359b1fc41f518a10
Ryan Kersten <ryankers@umich.edu>
2025-08-05 18:31:42 -0500

    Add real distance tests later

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -1,21 +1,16 @@
 import unittest
+from time import perf_counter
+
+from matplotlib import pyplot as plt
+
 import QEDV_lib.error_codes
 import QEDV_lib.verifier
+import pandas as pd
+
 class Test_Distance(unittest.TestCase):
 
     def setUp(self):
         ...
 
     def test_basic(self):
-        surface = QEDV_lib.error_codes.SurfaceCode(3)
-        print("Testing surface code of legnth 3")
-        print("X stabs are")
-        print(surface.x_stab)
-        print("Y stabs are")
-        print(surface.z_stab)
-        # val = QEDV_lib.verifier.minDistance(surface, 2)
-        # self.assertTrue(val)
-
-        print(QEDV_lib.verifier.distSearch(surface))
-        print(QEDV_lib.verifier.minDistance(surface,3))
-
+        ...
